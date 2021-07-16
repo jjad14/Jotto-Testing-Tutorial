de34571888e3142b66233979aff17646862f0928
Dennis <dmarlo96@live.ca>
2021-07-15 23:15:52 -0400

    Success Reducer test

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,5 +1,12 @@
-const reducer = (state, action) => {
-	return null;
+import { actionTypes } from "../actions";
+
+const reducer = (state = false, action) => {
+	switch (action.type) {
+		case actionTypes.CORRECT_GUESS:
+			return true;
+		default:
+			return state;
+	}
 };
 
 export default reducer;
---
+++ src/reducers/successReducer.test.js
@@ -0,0 +1,20 @@
+import { actionTypes } from "../actions";
+import successReducer from "./successReducer";
+
+test("when previous is undefined, return false", () => {
+	const newState = successReducer(undefined, {});
+
+	expect(newState).toBe(false);
+});
+
+test("return previous state when unknown action type", () => {
+	const newState = successReducer(false, { type: "unknown" });
+
+	expect(newState).toBe(false);
+});
+
+test("return true for action type CORRECT_GUESS", () => {
+	const newState = successReducer(false, { type: actionTypes.CORRECT_GUESS });
+
+	expect(newState).toBe(true);
+});
