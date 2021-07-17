f8b1ed1129561f0d918b91c44779721e5f3591c9
Dennis <dmarlo96@live.ca>
2021-07-16 21:14:16 -0400

    guessWords tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,13 +2,10 @@ import axios from "axios";
 
 export const actionTypes = {
 	CORRECT_GUESS: "CORRECT_GUESS",
+	GUESS_WORD: "GUESS_WORD",
 };
 
-export const correctGuess = () => {
-	return {
-		type: actionTypes.CORRECT_GUESS,
-	};
-};
+export const guessWord = (guessedWord) => (dispatch, getState) => {};
 
 export const getSecretWord = () => {
 	// TODO: write the actual action in Redux / Context section
